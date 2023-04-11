bac8c27198e0e915b3cebae4d058a1d0bc0f594c
Juan Senen <juansenen@hotmail.com>
2023-04-11 22:11:22 +0200

    Change view EditStation

--- app/src/main/java/com/example/transapp/view/EditStationView.java
+++ app/src/main/java/com/example/transapp/view/EditStationView.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 import com.example.transapp.R;
 import com.example.transapp.contract.EditStationContract;
@@ -43,7 +44,7 @@ public class EditStationView extends AppCompatActivity implements EditStationCon
     private boolean bus,taxi,wifi,ptoInfo;
     private double longitude,latitude;
     private long idStation;
-    private Button buttonEdit;
+    private ImageButton buttonEdit;
     private Stations stationBody;
 
     @Override
