13d9bade251626bf104eadd4fc1f6f478a2ba5a8
Juan Senen <juansenen@hotmail.com>
2023-04-12 09:51:29 +0200

    Change visual MArkets on MAP

--- app/src/main/java/com/example/transapp/view/UserSeeStationsView.java
+++ app/src/main/java/com/example/transapp/view/UserSeeStationsView.java
@@ -5,9 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.example.transapp.R;
 import com.example.transapp.contract.UserSeeStationsContract;
@@ -16,6 +19,8 @@ import com.example.transapp.presenter.UserSeeStationsPresenter;
 import com.mapbox.geojson.Point;
 import com.mapbox.maps.CameraOptions;
 import com.mapbox.maps.MapView;
+import com.mapbox.maps.MapboxMap;
+import com.mapbox.maps.extension.style.layers.properties.generated.TextAnchor;
 import com.mapbox.maps.plugin.annotation.AnnotationConfig;
 import com.mapbox.maps.plugin.annotation.AnnotationPlugin;
 import com.mapbox.maps.plugin.annotation.AnnotationPluginImplKt;
@@ -23,11 +28,14 @@ import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManager;
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationManagerKt;
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions;
 
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class UserSeeStationsView extends AppCompatActivity implements UserSeeStationsContract.View {
 
     private MapView mapView;
+    private MapboxMap mapboxMap;
     private PointAnnotationManager pointAnnotationManager;
     private UserSeeStationsPresenter presenter;
     private long idLine;
@@ -51,6 +59,8 @@ public class UserSeeStationsView extends AppCompatActivity implements UserSeeSta
         presenter = new UserSeeStationsPresenter(this,idLine,this);
         presenter.userLoadAllStations();
 
+
+
     }
     private void initializePointManager() {
         AnnotationPlugin annotationPlugin = AnnotationPluginImplKt.getAnnotations(mapView);
@@ -61,16 +71,20 @@ public class UserSeeStationsView extends AppCompatActivity implements UserSeeSta
         PointAnnotationOptions pointAnnotationOptions = new PointAnnotationOptions()
                 .withPoint(point)
                 .withTextField(title)
-                .withTextSize(14f)
-                .withTextColor(0xFF0000)
+                .withTextSize(20f)
+                .withTextColor(Color.BLUE)
+                .withTextAnchor(TextAnchor.TOP)
                 .withIconImage(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_station_gps_marker_foreground));
         pointAnnotationManager.create(pointAnnotationOptions);
+
+
     }
+
     private void setCameraPosition(Point point) {
         CameraOptions cameraPosition = new CameraOptions.Builder()
-                .center(point.fromLngLat(-74.0066, 40.7135))
+                .center(point)
                 .pitch(0.0)
-                .zoom(2.5)
+                .zoom(6.5)
                 .bearing(-17.6)
                 .build();
         mapView.getMapboxMap().setCamera(cameraPosition);
@@ -80,13 +94,15 @@ public class UserSeeStationsView extends AppCompatActivity implements UserSeeSta
             Point point = Point.fromLngLat(station.getLongitude(), station.getLatitude());
             addMarker(point, station.getName(), station.getId());
         }
+
     }
 
 
     @Override
     public void showAllStations(List<Stations> stationsList) {
-        addStationToMap(stationsList);
         centerMapOnPoints(stationsList);
+        addStationToMap(stationsList);
+
     }
 
     /** Menu barra de tareas */
@@ -122,5 +138,4 @@ public class UserSeeStationsView extends AppCompatActivity implements UserSeeSta
         Point point = Point.fromLngLat(lngAvg,latAvg);
         setCameraPosition(point);
     }
-
 }
